0fd2b6d46663af0e79255ba79ceefd3bb655e4a6
AneiMakovec <anei.makovec@hotmail.com>
2020-01-13 04:54:46 +0100

    Implemented falut tolerance, 3rd party API (weather) and event streaming.

--- interface/interface-persistence/src/main/java/com/acme/users/Favourite.java
+++ interface/interface-persistence/src/main/java/com/acme/users/Favourite.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import javax.persistence.*;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.As.WRAPPER_OBJECT;
